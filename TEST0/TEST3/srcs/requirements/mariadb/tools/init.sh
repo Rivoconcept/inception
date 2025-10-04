e79128d5fef0240af72d0e278014f3ddb374aa1d
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r12s9.42antananarivo.mg>
2025-10-04 16:24:27 +0300

    test3

---
+++ TEST0/TEST3/srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+DATA_DIR="/var/lib/mysql"
+
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+
+if [ ! -d "$DATA_DIR/mysql" ]; then
+    echo "[INFO] Initializing MariaDB..."
+    
+    mysql_install_db --user=mysql --datadir="$DATA_DIR" --skip-test-db
+    
+    mysqld --datadir="$DATA_DIR" --socket=/var/run/mysqld/mysqld.sock & MYSQL_PID=$!
+    
+    for i in {1..30}; do
+        if mysql -uroot -S /var/run/mysqld/mysqld.sock -e "SELECT 1" >/dev/null 2>&1; then
+            break
+        fi
+        sleep 1
+    done
+    
+    mysql -uroot -S /var/run/mysqld/mysqld.sock <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/MYSQL_PASSWORD)';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+UPDATE mysql.user SET password=PASSWORD('$(cat /run/secrets/MYSQL_ROOT_PASSWORD)') WHERE user='root';
+FLUSH PRIVILEGES;
+EOF
+
+    kill $MYSQL_PID
+    wait $MYSQL_PID
+    echo "[INFO] MariaDB initialized!"
+fi
+
+exec mysqld --datadir="$DATA_DIR"
